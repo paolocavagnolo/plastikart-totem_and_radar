5c3d08dbfe71a25c973f43d2e4ef4656ef5e800d
Paolo Cavagnolo <paolo.cavagnolo@gmail.com>
2021-09-18 19:25:10 +0200

    Create last_uno.ino

---
+++ radar/last_uno.ino
@@ -0,0 +1,34 @@
+#include <Conceptinetics.h>
+
+#define DMX_SLAVE_CHANNELS   4
+
+DMX_Slave dmx_slave ( DMX_SLAVE_CHANNELS );
+
+
+void setup() {
+
+  dmx_slave.enable ();
+
+  dmx_slave.setStartAddress (1);
+  pinMode(8,OUTPUT);
+  digitalWrite(8, HIGH);
+  pinMode(6,OUTPUT);
+  pinMode(7,OUTPUT);
+  digitalWrite(6,LOW);
+  digitalWrite(7,LOW);
+
+}
+
+int dmxMove;
+
+void loop()
+{
+
+  if (dmx_slave.getChannelValue(1) > 60) {
+    digitalWrite(8, HIGH);
+  }
+  else {
+    digitalWrite(8, LOW);
+  }
+  
+}
